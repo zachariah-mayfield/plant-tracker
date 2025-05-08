9dc591b3808cdccba4a61a30b18ee2771c04a866
zackmayfield <zachariah.mayfield@outlook.com>
2025-05-08 15:22:42 -0500

    Add get_db function to database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -17,3 +17,11 @@ else:
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    """Get a database session."""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
