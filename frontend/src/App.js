0202f2e9b074b9ceaa26c2b81c8309a594926359
zackmayfield <zachariah.mayfield@outlook.com>
2025-05-08 14:16:30 -0500

    Add React application files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Plant Tracker</h1>
+        <p>Welcome to your plant tracking application!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
